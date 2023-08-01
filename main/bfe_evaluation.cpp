0e374a4475f233717325406fef17ccd92b9b3b5f
Kit George <kit.george.21@ucl.ac.uk>
2023-08-01 20:24:16 +0100

    Actually, I'll just sample for a single phi

--- main/bfe_evaluation.cpp
+++ main/bfe_evaluation.cpp
@@ -93,20 +93,18 @@ int main() {
                       utility::flatten(coefficients));
 
     std::vector<std::function<double()>>
-    density_terms_functions((n_max + 1)*(2*l_max + 1)*N_R_values*N_phi_values/100);
+    density_terms_functions((n_max + 1)*(2*l_max + 1)*N_R_values);
     for(int i = 0; i <= n_max; ++i) {
         for(int j = 0; j < 2*l_max + 1; ++j) {
-            for(int k = 0; k < N_R_values/10; ++k) {
-                double R = (double)k*10/N_R_values*R_Ka;
-                for(int l = 0; l < N_phi_values/10; ++l) {
-                    double phi = (double)l*10/N_phi_values*2*std::numbers::pi;
-                    density_terms_functions[i*(2*l_max + 1)*N_R_values*N_phi_values/100
-                                            + j*N_R_values*N_phi_values/100
-                                            + k*N_phi_values/10 + l]
-                        = [&pot, R, phi, i, j] () {
-                        return std::abs(pot.density_terms[i][j](R, phi));
-                    };
-                }
+            for(int k = 0; k < N_R_values; ++k) {
+                double R = (double)k/N_R_values*R_Ka;
+                double phi = 0;
+                density_terms_functions[i*(2*l_max + 1)*N_R_values
+                                        + j*N_R_values
+                                        + k]
+                    = [&pot, R, phi, i, j] () {
+                    return std::abs(pot.density_terms[i][j](R, phi));
+                };
             }
         }
     }
