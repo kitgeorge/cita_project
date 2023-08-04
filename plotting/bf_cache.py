1ece6c0c3e0d63713b775499c954e9c0972cf087
Kit Christopher George <ctg2@msslanc.mssl.ucl.ac.uk>
2023-08-04 16:07:51 +0100

    Hopefully have correct plots now

--- plotting/bf_cache.py
+++ plotting/bf_cache.py
@@ -8,9 +8,9 @@ l_max = 50
 N_R_tabulated = 10000
 table_shape = (k_max + 1, n_max + 1, l_max + 1, N_R_tabulated)
 
-u_values = np.read_csv("../cache/basis_functions/u_values.csv", header=None).to_numpy()[:, 0]
-u_prime_values = np.loadtxt("../cache/basis_functions/u_prime_values.csv", header=None).to_numpy()[:, 0]
-d_values = np.loadtxt("../cache/basis_functions/d_values.csv", header=None).to_numpy()[:, 0]
+u_values = pd.read_csv("../cache/basis_functions/u_values.csv", header=None).to_numpy()[:, 0]
+u_prime_values = pd.read_csv("../cache/basis_functions/u_prime_values.csv", header=None).to_numpy()[:, 0]
+d_values = pd.read_csv("../cache/basis_functions/d_values.csv", header=None).to_numpy()[:, 0]
 u_values = u_values.reshape(table_shape)
 u_prime_values = u_prime_values.reshape(table_shape)
 d_values = d_values.reshape(table_shape)
@@ -19,6 +19,7 @@ k = 4
 l = 2
 
 for i in range(n_max + 1):
+    print(i)
     plt.figure()
     plt.plot(u_values[k, i, l, :])
     plt.savefig("../plots/bf_cache/u/k=4l=2/n={}.png".format(i), dpi=500)
