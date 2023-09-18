3bc991c1bc16d2d10adf14f93ba4995c75061193
Kit George <kit.george.21@ucl.ac.uk>
2023-09-18 14:57:25 -0400

    more

--- src/basis_functions/bfe.cpp
+++ src/basis_functions/bfe.cpp
@@ -348,7 +348,7 @@ std::vector<double> BFETables::getDValues() {
 ////////////////////////////////////////////////////////////
 
 double BFETables::getU(int n, int l, double R, double R_Ka) const {
-    std::abort();
+    std::cout << U_values[0] << std::endl;
     utility::SimpleTimer timer0;
     // utility::SimpleTimer timer1;
     // utility::SimpleTimer timer2;
